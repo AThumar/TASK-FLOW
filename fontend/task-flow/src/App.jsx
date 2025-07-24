0b784dddfcf110693f2c5a348167a8a0606344d7
Your Name <you@example.com>
2025-07-24 09:19:16 +0530

    Add PrivateRoute component for role-based access control

--- fontend/task-flow/src/App.jsx
+++ fontend/task-flow/src/App.jsx
@@ -13,6 +13,7 @@ import ManageUsers from "./pages/Admin/ManageUsers";
 import UserDashboard from "./pages/User/UserDashboard";
 import MyTasks from "./pages/User/MyTasks";
 import ViewTaskDetails from "./pages/User/ViewTaskDetails";
+import PrivateRoute from "./routes/PrivateRoute";
 
 const App = () => {
   return (
