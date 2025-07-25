1cdc931be88ecccf0171f98d96fd5f221763205e
Your Name <you@example.com>
2025-07-25 23:15:09 +0530

    Implement user registration functionality with role assignment and error handling

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,6 +14,34 @@ const generateToken = (userId) => {
 //@access Public
 const registerUser = async (req, res) => {
     try{
+const{name , email, password , profileImageUrl ,adminInviteToken} = req.body;
+const userExists = await User.findOne({ email });
+if(userExists){
+    return res.status(400).json({ message: 'User already exists' });
+}
+let role = 'member';
+if(adminInviteToken && adminInviteToken === process.env.ADMIN_INVITE_TOKEN) 
+    {   role = 'admin'; }
+
+const salt = await bcrypt.genSalt(10);
+const hashedPassword = await bcrypt.hash(password, salt);
+
+const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    profileImageUrl,
+    role,
+});
+
+res.status(201).json({
+    _id:user._id,
+    name:user.name,
+    email:user.email,
+    role:user.role,
+    profileImageUrl:user.profileImageUrl,
+    token:generateToken(user._id),
+});
 
     } catch (error) {
         console.error(error);
