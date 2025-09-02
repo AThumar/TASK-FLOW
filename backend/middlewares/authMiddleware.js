123f9cff44c573a797f84edbe907569057bba0e9
Your Name <you@example.com>
2025-09-02 09:34:03 +0530

    code

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 
 const protect = async (req, res, next) => {
     try{let token=req.headers.authorization;
-        if(token & token.startsWith('Bearer ')){
+        if(token && token.startsWith('Bearer ')){
             token = token.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select('-password');
@@ -15,7 +15,7 @@ const protect = async (req, res, next) => {
     }
     catch(err){
         console.error(err);
-        res.status(401).json({message: "Not authorized, token failed",error:error.message});
+        res.status(401).json({message: "Not authorized, token failed",error:err.message});
     }       
 };
 
