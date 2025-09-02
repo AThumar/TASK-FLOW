a08a64d457fd8811277aaf5b5c32e7bd2f0f9cf0
Your Name <you@example.com>
2025-09-02 20:30:51 +0530

    Implement code changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,7 +36,9 @@ const getUserById = async (req, res) => {try{
     const user = await User.findById(req.params.id).select('-password');
     if(!user){
         return res.status(404).json({message:"User not found"});
+        
     }
+    res.json(user);
 }
 catch (error) {
   
