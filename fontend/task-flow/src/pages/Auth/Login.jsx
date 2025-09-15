c0a86ff28c1ace002efd6eb54f832e0cac98f757
Your Name <you@example.com>
2025-09-15 23:08:18 +0530

    feat: add email validation and enhance Login component with error handling and improved UI

--- fontend/task-flow/src/pages/Auth/Login.jsx
+++ fontend/task-flow/src/pages/Auth/Login.jsx
@@ -3,6 +3,8 @@ import AuthLayout from "../../components/layouts/AuthLayout";
 import {useNavigate} from "react-router-dom";
 import { useState } from "react";
 import Input from "../../components/layouts/inputs/input";
+import { Link } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -12,6 +14,16 @@ const Login = () => {
 
     const handleLogin = async (e) => {
         e.preventDefault();
+
+        if(!validateEmail(email)){
+            setError("Please enter a valid email address");
+            return;
+        }
+        if(!password){
+            setError("Please enter your password");
+            return;
+        }
+        setError("");
     };
     return (
         <AuthLayout>
@@ -26,6 +38,18 @@ label="Email Address"
 placeholder="Enter your email"
 type = "text"
 ></Input>
+<Input value={password}
+onChange={({target})=>setPassword(target.value)}
+label="Password"
+placeholder="Min 8 characters"
+type = "password"
+></Input>
+{error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+<button type="submit" className="btn-primary">LOGIN</button>
+<p className="text-[13px] text-slate-800 mt-3">
+    Don't have an account?{" "}
+    <Link className="font-medium text-primary underline" to="/signup">SignUp</Link>
+</p>
                 </form>
             </div>
         </AuthLayout>
