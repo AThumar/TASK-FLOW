be7b22e69204870bae004c1f17dc0ddba071cd69
Your Name <you@example.com>
2025-09-23 21:15:23 +0530

    feat: add InfoCard component and integrate it into Dashboard for task statistics display

--- fontend/task-flow/src/pages/Admin/Dashboard.jsx
+++ fontend/task-flow/src/pages/Admin/Dashboard.jsx
@@ -6,6 +6,10 @@ import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
+import moment from "moment/moment";
+import InfoCard from "../../components/cards/InfoCard";
+import { IoMdCard } from "react-icons/io";
+import { addThousandSeparators } from "../../utils/helper";
 
 const Dashboard = () => {
         useUserAuth();
@@ -35,8 +39,41 @@ const Dashboard = () => {
             return( ) => {};
         }, []);
     return  <DashboardLayout activeMenu="Dashboard">
-        Admin Dashboard
-      
+        <div className="card my-5">
+            <div>
+                <div className="col-span-3">
+                    <h2 className="text-xl md:text-2xl">Hello !!! {user?.name}</h2>
+                    <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">{moment().format("dddd Do MMM YYYY")}</p>
+
+                </div>
+            </div>
+<div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5">
+    <InfoCard 
+    // icon = {<IoMdCard />  }
+    label = "Total Tasks"
+    value = {addThousandSeparators(dashboardData?.charts?.taskDistribution?.All || 0)}
+    color = "bg-primary"
+    ></InfoCard>
+     <InfoCard 
+    // icon = {<IoMdCard />  }
+    label = "Pending Tasks"
+    value = {addThousandSeparators(dashboardData?.charts?.taskDistribution?.Pending || 0)}
+    color = "bg-violet-500"
+    ></InfoCard>
+     <InfoCard 
+    // icon = {<IoMdCard />  }
+    label = "In Progress Tasks"
+    value = {addThousandSeparators(dashboardData?.charts?.taskDistribution?.InProgress || 0)}
+    color = "bg-cyan-500"
+    ></InfoCard>
+     <InfoCard 
+    // icon = {<IoMdCard />  }
+    label = "Completed Tasks"
+    value = {addThousandSeparators(dashboardData?.charts?.taskDistribution?.Completed || 0)}
+    color = "bg-lime-500"
+    ></InfoCard>
+</div>
+        </div>
         </DashboardLayout>
     ;
 };
